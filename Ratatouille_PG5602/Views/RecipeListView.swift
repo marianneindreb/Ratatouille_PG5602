cf4bf74ebf5d8ed85181439e793d161769142b34
Marianne <91476569+marianneindreb@users.noreply.github.com>
2023-11-16 21:48:59 +0100

    added assets and brand colors

---
+++ Ratatouille_PG5602/Views/RecipeListView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListView.swift
+//  Ratatouille_PG5602
+//
+//  Created by Marianne Indrebø on 16/11/2023.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeListView()
+}
---
+++ Ratatouille_PG5602/Views/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  Ratatouille_PG5602
+//
+//  Created by Marianne Indrebø on 16/11/2023.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
